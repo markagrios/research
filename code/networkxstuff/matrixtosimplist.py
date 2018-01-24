16eb82cc31ce39fe97e29a8963e4fce328d173bf
Mark Agrios <magrios@gmail.com>
2018-01-24 16:22:19 -0500

    I have a lot of work to do

--- code/networkxstuff/matrixtosimplist.py
+++ code/networkxstuff/matrixtosimplist.py
@@ -1,9 +1,13 @@
 import networkx as nx
 import numpy as np
+import matplotlib.pyplot as plt
 import csv
 
+# G = nx.DiGraph()
+G = nx.Graph()
+
 # with open("connection_matrices/"+matrix) as csvfile:
-matrix = "fivecyclewithacross.csv"
+matrix = "6weird.csv"
 with open(matrix) as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     a = list(readCSV)
@@ -12,3 +16,10 @@ for ri in range(len(a[0])):
     for ci in range(len(a[0])):
         if(a[ri][ci] != "0"):
             print(ri,ci)
+            G.add_edge(ri,ci)
+
+print(G.nodes())
+print(G.edges())
+# nx.draw(G)
+# plt.show()
+print(nx.find_cliques(G))
