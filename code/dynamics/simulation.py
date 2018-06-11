47a762be31926092129790e9d86fe921763b526b
Mark Agrios <magrios@gmail.com>
2018-06-11 15:55:14 -0400

    fixed the sync metric, does between 0 and pi now and sample he min and max from the last half of the simulation so the start up sync doesnt throw it off.

--- code/dynamics/simulation.py
+++ code/dynamics/simulation.py
@@ -65,7 +65,7 @@ def count_connections(matrix):
     return(n)
 
 def scaleToInterval(x,domainMin,domainMax):     # given an interval and value in the interval it scales it to be between 0 and 2pi
-    return(((x - domainMin)/(domainMax - domainMin))*2*cmath.pi)
+    return(((x - domainMin)/(domainMax - domainMin))*cmath.pi)
 
 def list_to_csv(data):
     with open("storeddata/lasttoinit.csv", "wb") as csv_file:
@@ -85,7 +85,7 @@ matrix = sys.argv[1] + ".csv"
 
 N = N_from_Matrix(matrix)                   # number of neurons in network
 N_syn = count_connections(matrix)           # number of synapses
-duration = 2000*ms                          # how long simuldurations runs
+duration = 2000*ms                          # how long simulations runs
 
 tau_param = {'tau': 1*ms}
 
@@ -121,13 +121,9 @@ connect_from_Matrix(matrix)
 M = StateMonitor(G, ['z', 'x'], record=True)
 
 ix = -0.5*(1+sqrt(5))
-init_cond = {'x': ix, 'y': 1-5*ix*ix, 'z': 2}
+init_cond = {'x': ix, 'y': 1-5*ix*ix, 'z': 1.7}
 neuron_pars = {'a': 1, 'b': 3.3, 'I': 2, 'c': 1, 'd': 5, 'r': 0.001, 's': 4, 'xR': -0.5*(1+sqrt(5))}
 coupling_pars = {'Vo': 2, 'lam': 10, 'thet': -0.6, 'g': 0.4}
-
-# Puts all the initial conditions in place.
-for ic,value in init_cond.items():
-    setattr(G,ic,[value for _ in range(N)])
 
 # These set the parameters to be homogeneous
 for param,value in neuron_pars.items():
@@ -139,18 +135,32 @@ for param,value in coupling_pars.items():
 
 #************ Set initial state variables of neurons ***************************
 
-zinit = []
+# Puts all the initial conditions in place.
+
+sv_list = ['x', 'y', 'z']
+sv_inits = []
+perturb = np.random.normal(0,1, (3,N))
+for i in range(len(init_cond.items())):
+    sv_inits.append(float(init_cond.items()[i][1]))
+
+print(sv_inits)
+print(perturb)
+print("---")
+
+# for normal distribution init cond
+# for i in range(3):
+#     setattr(G,sv_list[i],[init_cond[sv_list[i]] + (perturb[i][_]) for _ in range(N)])
+
+# uncomment this for loop for the other two init cond regimes
+for ic,value in init_cond.items():
+    setattr(G,ic,[value for _ in range(N)])
+
 setattr(G,'z',[1.7+(_*0.1) for _ in range(N)])        # for slightly different init cond
 # setattr(G,'z',[1.7 for _ in range(N)])                  # for uniform init cond
 
 start_time = TIME.time()
 
 run(duration)                # http://brian2.readthedocs.io/en/stable/reference/brian2.monitors.statemonitor.StateMonitor.html
-# store()
-# S.connect(i=0,j=3)
-# restore()
-# run(duration/2)
-
 
 ######################## PLOTTING ##############################################
 
@@ -174,16 +184,24 @@ for i in range(0,N):
 
 print("---")
 
-simMin = np.min(getattr(M,'z')[i])
-simMax = np.max(getattr(M,'z')[i])
-# print(simMin,simMax)
+num_timesteps = int(str(duration).split(".")[0]) * 100000
+simMinlist = []
+simMaxlist = []
+
+for i in range(N):
+    simMinlist.append(np.min(M[i].z[(num_timesteps/2):]))
+    simMaxlist.append(np.max(M[i].z[(num_timesteps/2):]))
+
+simMin = np.min(simMinlist)
+simMax = np.max(simMaxlist)
+print(simMin,simMax)                            # I think we should sample from like halfway through the simulation to the end because having the network synchronize screws up the min and max
 # print(scaleToInterval(0,simMin,simMax))
 
 scaledM = []
 
 for i in range(0,N):
     scaledM.append([])
-    for j in range(0, int(str(duration).split(".")[0]) * 100000/10):
+    for j in range(0, num_timesteps/10):
         scaledM[i].append(scaleToInterval(M[i].z[10*j], simMin, simMax))
 
 
