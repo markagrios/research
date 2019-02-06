4332b3d4460b7a740ba3e667846f85f6349fe1d2
Mark Agrios <magrios@gmail.com>
2019-02-05 21:50:32 -0500

    some good sims my dude

--- code/SPIKE/sim.py
+++ code/SPIKE/sim.py
@@ -24,17 +24,3 @@ for g in range(len(ablate_groups)):
 
 
 os.system("python plot.py " + matrix)
-
-
-# qcontinue = raw_input("continue with this ablation sequence? ")
-# if(qcontinue == "y"):
-#     os.system(command + " q []")           # just to stabilize the network
-#     os.system(command + " cont []")        # just to run it once
-#
-#     for i in range(len(ablation_list)):
-#         argpassed = "python ablate.py " + network + " cont " + ",".join(ablation_list[:i+1])
-#         print("Now ablating neuron: " + ablation_list[i])
-#         os.system(argpassed)
-#
-#
-#     os.system("python spikeplot.py " + network)
