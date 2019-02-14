d7c8205920dc3e693f1d770e75b91ff9e53fb20b
Mark Agrios <magrios@gmail.com>
2019-02-14 17:23:04 -0500

    need to run a big sim to test if any huge sync stuff gets weird. then run many sims on many networks to gather data

--- code/SPIKE/keepdata.py
+++ code/SPIKE/keepdata.py
@@ -19,7 +19,7 @@ homXS = pickle.load(open("storeddata/" + matrix + "-homXS.p", "rb"))
 duration = len(train[0])
 singlerun = duration/(len(homXS)+2)
 
-print("--- plotting ---")
+print("--- storing data ---")
 
 # create list of spike times from raster
 spiketimes = []
@@ -31,61 +31,12 @@ for n in range(len(train)):
 
 st = th.make_trains(spiketimes)
 
-simulation = plt.figure(figsize=(17,10))
-simulation.add_subplot(4,1,1)
-th.plot_raster(st)
-for i in range(2,duration/singlerun):
-    if(homXS[i-2][1] == 'X'):
-        plt.axvline(x=(i)*singlerun, color='g', linestyle='--')
-    elif(homXS[i-2][1] == 'S'):
-        plt.axvline(x=(i)*singlerun, color='r', linestyle='--')
-    else:
-        plt.axvline(x=(i)*singlerun, color='b', linestyle='--')
-
-    plt.plot(i*singlerun, int(homXS[i-2][0]), marker="x", color="black")
-plt.xlabel('')
-
-
-
-# histogram for spikes
-simulation.add_subplot(4,1,2)
-spikeHist = th.psth(st, duration/1000)
-plt.plot(spikeHist[0])
-sys.stdout.write('\x1b[1A')
-sys.stdout.write('\x1b[2K')
-plt.ylabel("PST histogram")
-plt.xlim(xmin= 0,xmax=1000)
-plt.xticks([])
-
-# betti number plot
 difbetas = []
 for i in range(len(betas)):
     difbetas.append([])
     for c in range(len(betas[i])-1):
         difbetas[i].append(betas[i][c+1]-betas[i][c])
 
-homdec = []
-for b in range(len(difbetas)):
-    homdec.append([])
-    for i in range(len(difbetas[b])+1):
-        homdec[b].append(sum(difbetas[b][:i]))
-
-print(homdec[0])
-print(homdec[1])
-print(homdec[2])
-
-simulation.add_subplot(4,1,3)
-plt.ylabel("Change in Betti numbers")
-plt.plot(homdec[0], linestyle="--", marker="o", label="beta 0")
-plt.plot(homdec[1], linestyle="--", marker="o", label="beta 1")
-plt.plot(homdec[2], linestyle="--", marker="o", label="beta 2")
-plt.legend(loc='upper right')
-plt.xlim(xmin=-1.5,xmax=len(homdec[0])-0.5)
-
-
-# synchrony
-simulation.add_subplot(4,1,4)
-plt.ylabel("synchronization")
 
 chunk = duration/singlerun
 # print(chunk)
@@ -109,7 +60,7 @@ plt.plot(sync, linestyle="-", marker="o", markersize="7")
 plt.grid(which='both', axis='y')
 plt.xlim(xmin=-0.5,xmax=len(homXS)+1.5)
 for i in range(len(sync)):
-    plt.text(i, sync[i]-0.3, str(round(sync[i],2)))
+    plt.text(i+0.3, sync[i]+0.3, str(round(sync[i],2)))
 
 
 
